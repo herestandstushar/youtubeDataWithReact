ae76ad34ec7788d0a46303c70acbf6460f9b80e0
unknown <tomertusharthakur@gmail.com>
2022-03-12 12:55:45 +0530

    initilize commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import DataVisualization from './components/dataVisualization';
 
-function App() {
+
+// const Hh = (props)=>
+// {
+//   console.log(props);
+// }  
+
+
+const  App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DataVisualization></DataVisualization>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/dataVisualization.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DataVisualization = () => {
+  return (
+    <div>
+        <img src ='C:\Users\Tushar Tomar\Downloads\surprice\__results___23_0.png' alt = 'vislua'></img>
+    </div>
+  )
+}
+
+
+export  default DataVisualization
